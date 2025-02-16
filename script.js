a37d43926b810f216a78ab30acb9177606df5bae
HamBird <122409142+HamBird@users.noreply.github.com>
2025-02-16 01:43:15 -0700

    Shuffled Functions
    Combined both newGame() and startGame(), added a new property for player wins

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// disable start button til players are added
+document.querySelector(".restart").setAttribute("disabled", true);
+
 const Gameboard = (function () {
     // gameboard array that will hold game data
     var gameArray = [];
@@ -30,10 +33,13 @@ const Gameboard = (function () {
 })();
 
 function Player(name, marker) {
+    let winCount = 0;
     const getName = () => name;
     const getMarker = () => marker;
+    const incrementWin = () => winCount++;
+    const getWins = () => winCount;
 
-    return { getName, getMarker };
+    return { getName, getMarker, getWins, incrementWin };
 }
 
 const displayController = (function () {
@@ -70,11 +76,12 @@ const displayController = (function () {
 
 const gameFlow = (function () {
     // stores active players
-    let players = [
-        // preset players to test UI
-        Player("Joe", "X"),
-        Player("Bill", "O"),
-    ];
+    var players = [];
+    // let players = [
+    //     // preset players to test UI
+    //     Player("Joe", "X"),
+    //     Player("Bill", "O"),
+    // ];
 
     let currentPlayer;
     // allows for new players to be registered, cannot allow more than two players
@@ -111,7 +118,9 @@ const gameFlow = (function () {
         }
         return isWon;
     }
-    var isGameOver = false;
+
+    // var isGameOver = false;
+
     // checks the gameboard to see if there are any more valid cells left over
     const checkTie = () => {
         const board = Gameboard.getGameboard();
@@ -123,14 +132,14 @@ const gameFlow = (function () {
         displayController.displayStatus(`${currentPlayer.getName()}'s Turn!`);
     }
 
-    const startGame = () => {
-        if (players.length < 2 && !isGameOver) return;
-        currentPlayer = players[0];
-        newRound();
-    }
+    // const startGame = () => {
+    //     if (players.length < 2 && !isGameOver) return;
+    //     currentPlayer = players[0];
+    //     newRound();
+    // }
 
-    // used to debug UI
-    startGame();
+    // // used to debug UI
+    // startGame();
     const playRound = (row, col) => {
         // need to check valid placement
         if (Gameboard.placeMarker(currentPlayer.getMarker(), row, col) && !isGameOver) {
@@ -139,6 +148,7 @@ const gameFlow = (function () {
             if (checkWinCond(currentPlayer.getMarker())) {
                 console.log(`Player ${currentPlayer.getName()} has won!`);
                 displayController.displayStatus(`Player ${currentPlayer.getName()} has won!`);
+                currentPlayer.incrementWin();
                 isGameOver = true;
             }
             else if (checkTie()) {
@@ -154,18 +164,35 @@ const gameFlow = (function () {
         }
     }
 
-    const newGame = (newPlayers) => {
+    const newGame = () => {
+        if (players.length < 2) return;
         Gameboard.resetGameboard();
         displayController.displayBoard();
-        displayController.displayStatus("Start game when ready!");
+        currentPlayer = players[0];
+        newRound();
         isGameOver = false;
-        if (newPlayers === true) {
-            players = [];
-        }
     }
+    // const newGame = (newPlayers) => {
+    //     Gameboard.resetGameboard();
+    //     displayController.displayBoard();
+    //     displayController.displayStatus("Start game when ready!");
+    //     isGameOver = false;
+    //     if (newPlayers === true) {
+    //         players = [];
+    //     }
+    // }
 
-    // Should add a boolean to check if game has ended in anyway to halt the game from progressing anymore
-    return { regNewPlayers, newGame, playRound, startGame };
+    // Should remove the fact of using newGame() and startGame() and just combine them for the restart button combination and
+    //  inside newplayers dialog, if the user inserts two names and hits "submit" remove existing players and make new players
+    //   or if the user closes the dialog, keep the existing players.
+
+    // Also for the players object, include a property to increment wins so that there may be a scoreboard for wins on the HTML
+
+    // Also for the "Start/Restart" button, disable the button and stlye using :disabled, so that onload the button cannot be pressed
+    //  until there are two valid players
+
+    // return { regNewPlayers, playRound, startGame, newGame};
+    return { regNewPlayers, playRound, newGame };
 })();
 
 
